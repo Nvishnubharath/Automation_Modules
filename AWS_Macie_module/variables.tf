dc475708a1430c7043782bcd436ea7026bad6827
Nvishnubharath <153408042+Nvishnubharath@users.noreply.github.com>
2024-06-11 11:41:54 +0530

    Add files via upload

---
+++ AWS_Macie_module/variables.tf
@@ -0,0 +1,20 @@
+variable "team" {
+  description = "Team name"
+  type = string
+  default = "CloudOps"
+}
+variable "environment" {
+  description = "Environment name"
+  type = string
+  default = "Practice"
+}
+variable "owner" {
+  description = "Owner name"
+  type = string
+  default = "Vishnu Bharath Narandas"
+}
+variable "owner_email" {
+  description = "Owner email"
+  type = string
+  default = "narandasvishnubharath@gmail.com"
+}
